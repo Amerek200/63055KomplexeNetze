34d52e02f5a2574976c25edc664835be9745a5f4
Sebatian Dietze <sebastiandietze@gmx.net>
2025-06-10 19:00:04 +0200

    combine_file fix

--- combine_files.py
+++ combine_files.py
@@ -2,15 +2,16 @@
 from os import listdir
 from os.path import isfile, join
 
-DATA_PATH = "data/input/"
+DATA_PATH = "data/input"
 
 # Liste mit Dateinamen von Büchern
-file_name_list = [f for f in listdir(DATA_PATH) if isfile(join(DATA_PATH, f)) and "cleaned" in f]
+file_name_list = [f for f in listdir(DATA_PATH) if isfile(join(DATA_PATH, f)) and f.endswith("_de.txt")]
 
 # Liste mit Inhalten von Dateien
 file_content_list = [" ".join(open(join(DATA_PATH, f)).readlines()) + " " for f in file_name_list]
 
+result_path = join(DATA_PATH, "ALL_ALL_DE.txt")
 
-with open(DATA_PATH + "ALL_ALL_DE.txt", "w") as f:
+with open(result_path, "w") as f:
     for content in file_content_list:
         f.writelines(content)
