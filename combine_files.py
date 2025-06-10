1b5209c498dc770bad511aca14dd864e4ff09f55
Lezin901 <l.axmann901@gmail.com>
2025-06-10 15:52:26 +0200

    Lambda Bestimmung in Analyse über Dataframe eingebaut und externe Datei für Bestimmung von Lambda wieder aus Projektordner entfernt

---
+++ combine_files.py
@@ -0,0 +1,16 @@
+# Allgemeiner Pfad zu den Daten
+from os import listdir
+from os.path import isfile, join
+
+DATA_PATH = "data/input/"
+
+# Liste mit Dateinamen von Büchern
+file_name_list = [f for f in listdir(DATA_PATH) if isfile(join(DATA_PATH, f)) and "de_cleaned" in f]
+
+# Liste mit Inhalten von Dateien
+file_content_list = [" ".join(open(join(DATA_PATH, f)).readlines()) for f in file_name_list]
+
+
+with open(DATA_PATH + "all_in_all.txt", "w") as f:
+    for content in file_content_list:
+        f.writelines(content)
