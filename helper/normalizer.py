3d4131d9ebf7ddb90e54d06ced19744061e5b322
Lezin901 <l.axmann901@gmail.com>
2025-07-19 12:55:28 +0200

    Normalisierung angepasst. Link-Distanz auf 1 gestellt bei mixed_languages

--- helper/normalizer.py
+++ helper/normalizer.py
@@ -1,5 +1,16 @@
-def normalize(df, features):
-    normalized_df = df
-    normalized_df[features] = (df[features] - df[features].min()) / (df[features].max() - df[features].min())
+# def normalize(df, features):
+#     normalized_df = df
+#     normalized_df[features] = (df[features] - df[features].min()) / (df[features].max() - df[features].min())
+#
+#     return normalized_df
 
+def normalize(df, features):
+    normalized_df = df.copy()
+    for feature in features:
+        min_val = df[feature].min()
+        max_val = df[feature].max()
+        if max_val == min_val:
+            normalized_df[feature] = min_val
+        else:
+            normalized_df[feature] = (df[feature] - min_val) / (max_val - min_val)
     return normalized_df
